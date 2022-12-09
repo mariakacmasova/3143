d6a95fa068ecf45e78cdeed5da0b490ffd1e162b
mariakacmasova <105441119+mariakacmasova@users.noreply.github.com>
2022-12-10 00:05:23 +0300

    Update settings.go

--- src/config/settings.go
+++ src/config/settings.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	AppVersion             = "v4.0.0"
-	AppPort                = "3000"
+	AppPort                = "3143"
 	AppDebug               = true
 	AppOs                  = fmt.Sprintf("Chrome")
 	AppPlatform            = waProto.DeviceProps_PlatformType(1)
